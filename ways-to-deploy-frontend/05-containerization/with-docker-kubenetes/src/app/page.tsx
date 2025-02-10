5023450282611a7e281f1627688af2c4f1bfd6bf
hareesh <hareeshbhittam@gmail.com>
2025-02-10 11:14:44 +0530

    with-docker-kubenetes app

---
+++ ways-to-deploy-frontend/05-containerization/with-docker-kubenetes/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-lvh">
+      <div className="font-bold border-4 rounded-lg p-3">
+        Hello, Welcome to the my world!
+      </div>
+    </div>
+  );
+}
