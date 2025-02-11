b085dc098dc696c837933c56feb11a865a64003a
hareesh <hareeshbhittam@gmail.com>
2025-02-11 09:44:14 +0530

    Created sample application

---
+++ ways-to-deploy-frontend/03-traditional-web-servers/nginx/nginx-local/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-lvh">
+      <p className="font-bold p-4 border-4 rounded-lg cursor-pointer ">
+        Welcome to the my world!
+      </p>
+    </div>
+  );
+}
