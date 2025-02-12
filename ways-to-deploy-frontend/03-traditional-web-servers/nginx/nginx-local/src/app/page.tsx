99ce195ae8b2bbc8b00f1b24bebef544c7fd7926
hareesh <hareeshbhittam@gmail.com>
2025-02-12 15:16:51 +0530

    Minor change in UI

--- ways-to-deploy-frontend/03-traditional-web-servers/nginx/nginx-local/src/app/page.tsx
+++ ways-to-deploy-frontend/03-traditional-web-servers/nginx/nginx-local/src/app/page.tsx
@@ -1,9 +1,21 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-lvh">
-      <p className="font-bold p-4 border-4 rounded-lg cursor-pointer ">
-        Welcome to the my world!
-      </p>
+      <div className="font-bold p-4 border-4 rounded-lg cursor-pointer">
+        <p className="font-bold p-4 border-4 rounded-lg cursor-pointer text-center">
+          {" "}
+          Hii, This is Hareesh!
+        </p>
+        <p className="font-bold p-4 border-4 rounded-lg cursor-pointer text-center">
+          What are doing here.
+        </p>
+        <p className="font-bold p-4 border-4 rounded-lg cursor-pointer text-center">
+          Have you got lost?
+        </p>
+        <p className="font-bold p-4 border-4 rounded-lg cursor-pointer text-center">
+          Anyway welcome to the Nginx!
+        </p>
+      </div>
     </div>
   );
 }
